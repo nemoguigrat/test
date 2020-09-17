0c6f7abf2e0ff2123844b79c54b3d17d162a3a61
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-09-17 19:07:35 +0500

    Added "Дима чмо"

--- test/Program.cs
+++ test/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace test
+﻿using System;
+
+namespace test
 {
     internal class Program
     {
         public static void Main(string[] args)
-        {
+        {    
+            Console.WriteLine("Дима чмо");
         }
     }
 }
